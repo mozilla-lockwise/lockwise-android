416ecb8e7d333f904fbefab6aefad97b16902b55
Devin Reams <dreams@mozilla.com>
2018-08-14 15:02:25 -0600

    Rename home display label and activity full app name

--- app/src/main/java/com/firefox/lockbox/firefoxlockbox/MainActivity.kt
+++ app/src/main/java/com/firefox/lockbox/firefoxlockbox/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.title = getString(R.string.app_name)
     }
 }
