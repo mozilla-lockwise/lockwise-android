9770c4b66cfa60f2905463b6080237987b8bb2aa
sashei <sashaheinen@gmail.com>
2018-09-17 16:06:43 -0600

    add license

--- app/src/main/java/mozilla/lockbox/MainActivity.kt
+++ app/src/main/java/mozilla/lockbox/MainActivity.kt
@@ -1,5 +1,8 @@
-package mozilla.lockbox
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+package mozilla.lockbox
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
