d95a1cbc69c350424f3f8ac1f6200640d38ec154
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-09-17 16:46:46 -0600

    initial drawer implementation

--- app/src/main/java/mozilla/lockbox/view/MainActivity.kt
+++ app/src/main/java/mozilla/lockbox/view/MainActivity.kt
@@ -4,17 +4,53 @@
 
 package mozilla.lockbox.view
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.widget.TextView
 import mozilla.lockbox.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setup drawer interactions
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            title = getString(R.string.app_name)
+        }
+        drawer = findViewById(R.id.app_drawer)
+
+        val nav: NavigationView = findViewById(R.id.nav_view)
+        nav.setNavigationItemSelectedListener { menuItem ->
+            drawer.closeDrawers()
+
+            // TODO: wire up actions to navigation selections
+
+            true
+        }
+
+        // setup content
         val textView = this.findViewById<TextView>(R.id.hello_world)
         textView.setText(R.string.hello_world)
-        this.supportActionBar?.title = getString(R.string.app_name)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
