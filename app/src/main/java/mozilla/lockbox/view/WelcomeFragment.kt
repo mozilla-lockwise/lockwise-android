1f857c82cc9de44135416950eda067439828f2cf
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-09-18 12:53:46 -0600

    Root Activity + Welcome Fragment (#78)
    * root activity + initial Welcome Fragment
    
    * address linting and CR comments

---
+++ app/src/main/java/mozilla/lockbox/view/WelcomeFragment.kt
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.lockbox.view
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import mozilla.lockbox.R
+
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.welcome_fragment, container, false)
+
+        // TODO: a better parameterized string formatter?
+
+        val btn: Button = view.findViewById(R.id.getstarted_btn)
+        btn.setOnClickListener {
+            // TODO: dispatch "welcome" (or "fxa"?) action
+        }
+
+        return view
+    }
+}
