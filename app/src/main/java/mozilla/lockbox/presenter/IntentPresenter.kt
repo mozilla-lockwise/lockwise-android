94b3cac4d92db28c73889ba46b6227e25ac7e4a9
sashei <sashaheinen@gmail.com>
2018-09-18 14:27:54 -0600

    swapping fragments in root, back stack manipulation

--- app/src/main/java/mozilla/lockbox/presenter/IntentPresenter.kt
+++ app/src/main/java/mozilla/lockbox/presenter/IntentPresenter.kt
@@ -1,20 +1,60 @@
 package mozilla.lockbox.presenter
 
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import mozilla.lockbox.R
 import mozilla.lockbox.action.RouteAction
-import mozilla.lockbox.flux.Dispatcher
+import mozilla.lockbox.action.RouteAction.*
 import mozilla.lockbox.store.RouteStore
+import mozilla.lockbox.view.FxALoginFragment
+import mozilla.lockbox.view.WelcomeFragment
 
-class IntentPresenter(routeStore: RouteStore = RouteStore.shared) {
-    companion object {
-        val shared = IntentPresenter()
-    }
+
+class IntentPresenter(private val activity: AppCompatActivity, routeStore: RouteStore = RouteStore.shared) {
+
+    val welcome: WelcomeFragment by lazy { WelcomeFragment() }
+    val login: FxALoginFragment by lazy { FxALoginFragment() }
 
     init {
         routeStore.routes.subscribe { a -> route(a) }
     }
 
+    fun onViewReady() {
+        replaceFragment(this.welcome, false)
+    }
+
+    private fun replaceFragment(frag: Fragment, backable: Boolean = true) {
+        val tx = activity.supportFragmentManager.beginTransaction()
+        tx.replace(R.id.root_content, frag)
+        if (backable) {
+            tx.addToBackStack(null)
+        }
+        tx.commit()
+    }
+
+    private fun clearBackStack() {
+        val fm = activity.supportFragmentManager
+        if (fm.backStackEntryCount > 0) {
+            val base = fm.getBackStackEntryAt(0)
+            fm.popBackStackImmediate(base.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+    }
+    
     fun route(action: RouteAction) {
         Log.i("dfsadfs", "action: ${action.name}")
+        when (action) {
+            LOGIN -> {
+                replaceFragment(login)
+            }
+            WELCOME -> {
+                clearBackStack()
+                replaceFragment(welcome, false)
+            }
+            ITEMLIST -> {
+                clearBackStack()
+            }
+        }
     }
 }
---
+++ app/src/main/java/mozilla/lockbox/view/FxALoginFragment.kt
@@ -0,0 +1,32 @@
+package mozilla.lockbox.view
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import mozilla.lockbox.R
+class FxALoginFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fxa_login, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
