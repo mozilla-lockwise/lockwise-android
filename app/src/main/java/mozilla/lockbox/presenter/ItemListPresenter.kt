a8d2a03f6fb140b6c4211818d47204c851757d11
James Hugman <james@hugman.tv>
2018-09-19 15:58:34 -0600

    Add disposables to ItemListPresenter.

--- app/src/main/java/mozilla/lockbox/presenter/ItemListPresenter.kt
+++ app/src/main/java/mozilla/lockbox/presenter/ItemListPresenter.kt
@@ -6,6 +6,7 @@ package mozilla.lockbox.presenter
 
 import android.view.MenuItem
 import io.reactivex.Observable
+
 import mozilla.lockbox.flux.Presenter
 
 interface ListEntriesProtocol {
@@ -14,10 +15,10 @@ interface ListEntriesProtocol {
     // TODO: Item list selection
 }
 
+
 class ListEntriesPresenter(private val protocol: ListEntriesProtocol): Presenter() {
     override fun onViewReady() {
         // TODO: register for drawer item selections
 
         // TODO: register for item list selections
     }
-}
