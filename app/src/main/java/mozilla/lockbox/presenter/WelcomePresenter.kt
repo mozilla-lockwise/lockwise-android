76a206c6a2cfa6a6b3d1ba057c4727f4dac54e34
James Hugman <james@hugman.tv>
2018-09-19 15:58:04 -0600

    Tidy Welcome Fragment and presenter.

--- app/src/main/java/mozilla/lockbox/presenter/WelcomePresenter.kt
+++ app/src/main/java/mozilla/lockbox/presenter/WelcomePresenter.kt
@@ -1,2 +1,26 @@
 package mozilla.lockbox.presenter
 
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import mozilla.lockbox.action.RouteAction
+import mozilla.lockbox.flux.Dispatcher
+
+interface WelcomeViewProtocol {
+    val getStartedClicks: Observable<Unit>
+}
+
+class WelcomePresenter(private val protocol: WelcomeViewProtocol, val dispatcher: Dispatcher = Dispatcher.shared) {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    fun onViewReady() {
+        protocol.getStartedClicks.subscribe { _ ->
+            dispatcher.dispatch(RouteAction.LOGIN)
+        }.addTo(compositeDisposable)
+    }
+
+    fun onDestroy() {
+        compositeDisposable.clear()
+    }
+}
