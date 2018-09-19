e0fa2a1de3961f0ef965cb7985492480dd05a38e
James Hugman <james@hugman.tv>
2018-09-19 12:57:17 -0600

    Add DispatcherTest

---
+++ app/src/test/java/mozilla/lockbox/DispatcherTest.kt
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.lockbox
+
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import mozilla.lockbox.flux.Dispatcher
+import mozilla.lockbox.flux.Action
+import org.junit.Test
+import java.util.concurrent.atomic.AtomicBoolean
+
+private enum class TestAction : Action {
+    UNIT
+}
+
+class DispatcherTest {
+
+    @Test
+    fun testSingleObserver() {
+        val dispatcher = Dispatcher()
+        val onNextExecuted = AtomicBoolean(false)
+
+        val subscription = dispatcher.register.subscribe { _ -> onNextExecuted.set(true) }
+        assertFalse(onNextExecuted.get())
+        dispatcher.dispatch(TestAction.UNIT)
+        assertTrue(onNextExecuted.get())
+
+        subscription.dispose()
+    }
+
+    @Test
+    fun testMultipleObserver() {
+        val dispatcher = Dispatcher()
+        val num = 10
+
+        val executedFlags = Array(num, { _ -> AtomicBoolean(false) })
+        val subscriptions = executedFlags.map { f -> dispatcher.register.subscribe { _ -> f.set(true) }}
+
+        executedFlags.forEach { b -> assertFalse(b.get()) }
+        dispatcher.dispatch(TestAction.UNIT)
+
+        executedFlags.forEach { b -> assertTrue(b.get()) }
+        subscriptions.forEach { s -> s.dispose() }
+    }
+}
