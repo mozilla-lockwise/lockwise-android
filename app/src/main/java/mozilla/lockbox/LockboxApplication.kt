d522d550be7208ad9861da60dcf558d8c4ad2a79
Devin Reams <dreams@mozilla.com>
2018-09-24 14:41:07 -0600

    add Sentry to the project

--- app/src/main/java/mozilla/lockbox/LockboxApplication.kt
+++ app/src/main/java/mozilla/lockbox/LockboxApplication.kt
@@ -10,11 +10,18 @@ import android.app.Application
 import mozilla.components.support.base.log.Log
 import mozilla.components.support.base.log.logger.Logger
 import mozilla.components.support.base.log.sink.AndroidLogSink
+import io.sentry.android.AndroidSentryClientFactory
+import io.sentry.Sentry
 
 val log: Logger = Logger("Lockbox")
 class LockboxApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Log.addSink(AndroidLogSink())
+
+        // Set up using Sentry DSN (client key) from the Project Settings page on Sentry
+        val ctx = this.applicationContext
+        val sentryDsn = "https://19558af5301f43e1a95ab4b8ceae663b:d3f7f6dfa9114ef0ba58accb8d8cc1ad@sentry.prod.mozaws.net/401"
+        Sentry.init(sentryDsn, AndroidSentryClientFactory(ctx))
     }
 }
