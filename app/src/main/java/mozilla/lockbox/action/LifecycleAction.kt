6efe7c4d00d290ff204a097801f35a17f1aee392
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-10-10 17:12:02 -0600

    basisc for Lifecycle

---
+++ app/src/main/java/mozilla/lockbox/action/LifecycleAction.kt
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package mozilla.lockbox.action
+
+enum class LifecycleAction(
+    override val eventMethod: TelemetryEventMethod,
+    override val eventObject: TelemetryEventObject
+) : TelemetryAction {
+    Foreground(TelemetryEventMethod.foreground, TelemetryEventObject.app),
+    Background(TelemetryEventMethod.background, TelemetryEventObject.app),
+    Startup(TelemetryEventMethod.startup, TelemetryEventObject.app),
+    // TODO: Add a TelemetryEventMethod for upgrading
+    Upgrade(TelemetryEventMethod.startup, TelemetryEventObject.app)
+}
