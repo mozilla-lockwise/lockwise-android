a8d4795c9fef7eff71afa3bae776f77eadd179d3
James Hugman <james@hugman.tv>
2018-10-15 15:52:36 +0100

    Initial commit.

---
+++ app/src/main/java/mozilla/lockbox/support/DebugSupport.kt
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package mozilla.lockbox.support
+
+import android.os.Looper
+import mozilla.lockbox.BuildConfig
+
+fun isOnUiThread(): Boolean {
+    return Looper.getMainLooper() == Looper.myLooper()
+}
+
+fun isDebug(): Boolean {
+    return BuildConfig.DEBUG
+}
+
+fun assertOnUiThread(detailMessage: String = "Should be on the UI thread") {
+    if (isDebug() && !isOnUiThread()) {
+        throw AssertionError(detailMessage)
+    }
+}
+
+fun assertNotOnUiThread(detailMessage: String = "Should not be on the UI thread") {
+    if (isDebug() && isOnUiThread()) {
+        throw AssertionError(detailMessage)
+    }
+}
