bd0d61f0f124313883202c15af1a7b129da2c77b
eliserichards <erichards@mozilla.com>
2018-12-11 16:32:02 -0800

    Opens fragment successfully for both views, but no web page.

--- app/src/main/java/mozilla/lockbox/presenter/WebViewPresenter.kt
+++ app/src/main/java/mozilla/lockbox/presenter/WebViewPresenter.kt
@@ -7,11 +7,11 @@
 package mozilla.lockbox.presenter
 
 import android.support.annotation.IdRes
-import android.support.annotation.StringRes
 import io.reactivex.Observable
 import io.reactivex.functions.Consumer
-import io.reactivex.rxkotlin.addTo
 import mozilla.lockbox.R
+import mozilla.lockbox.action.AccountAction
+import mozilla.lockbox.action.DataStoreAction
 import mozilla.lockbox.action.RouteAction
 import mozilla.lockbox.flux.Dispatcher
 import mozilla.lockbox.flux.Presenter
@@ -19,7 +19,8 @@ import mozilla.lockbox.log
 import mozilla.lockbox.support.Constant
 
 interface NewWebView {
-    val menuListener: Observable<Int>
+    var menuObserver: Consumer<String?>?
+    fun loadUrl(url: String)
 }
 
 class WebViewPresenter(
@@ -28,17 +29,16 @@ class WebViewPresenter(
 ) : Presenter() {
 
     override fun onViewReady() {
-        view.menuListener
-            .subscribe(this::onMenuItem)
-            .addTo(compositeDisposable)
-    }
-
-    private fun onMenuItem(@IdRes itemId: Int) {
-        val uri = when (itemId) {
-            R.id.faq_menu_item -> Constant.Faq.uri
-            R.id.feedback_menu_item -> Constant.FeedbackLink.uri
-            else -> return log.error("Cannot route from list item.")
+        view.menuObserver = Consumer { item ->
+            item?.let {
+                when {
+                    item.startsWith(Constant.Faq.uri) ->
+                        dispatcher.dispatch(RouteAction.OpenWebsite(Constant.Faq.uri))
+                    item.startsWith(Constant.FeedbackLink.uri) ->
+                        dispatcher.dispatch(RouteAction.OpenWebsite(Constant.FeedbackLink.uri))
+                    else -> log.error("Cannot route from list item.")
+                }
+            }
         }
-        dispatcher.dispatch(RouteAction.OpenWebsite(uri))
     }
 }
--- app/src/main/java/mozilla/lockbox/view/WebViewFragment.kt
+++ app/src/main/java/mozilla/lockbox/view/WebViewFragment.kt
@@ -14,36 +14,40 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import com.jakewharton.rxbinding2.support.design.widget.itemSelections
-import io.reactivex.Observable
 import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.fragment_item_list.view.*
 import kotlinx.android.synthetic.main.fragment_webview.*
 import mozilla.lockbox.R
 import mozilla.lockbox.presenter.WebViewPresenter
 import mozilla.lockbox.presenter.NewWebView
-import mozilla.lockbox.support.Constant
 
 
 class WebViewFragment : BackableFragment(), NewWebView {
+    override var menuObserver: Consumer<String?>? = null
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        val itemSelection = view!!.navView.itemSelections().map { it.itemId }.
         presenter = WebViewPresenter(this)
         return inflater.inflate(R.layout.fragment_webview, container, false)
     }
 
-    override val menuListener: Observable<Int>
-        get() {
-            val navView = view!!.navView
-            return navView.itemSelections().map { it.itemId }
-        }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.navToolbar.setNavigationIcon(R.drawable.ic_close)
+//        view.navToolbar.setNavigationIcon(R.drawable.ic_close)
+    }
+
+    override fun loadUrl(url: String) {
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                menuObserver?.accept(url)
+                super.onPageStarted(view, url, favicon)
+            }
+        }
+        webView.loadUrl(url)
     }
 }
