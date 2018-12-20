ccc1a9e4f5ab97f1825e532ec3116ffd0f2da529
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-12-20 12:18:22 -0700

    Capture OAuth Redirect before it can be Displayed (#306)

--- app/src/test/java/mozilla/lockbox/presenter/FxALoginPresenterTest.kt
+++ app/src/test/java/mozilla/lockbox/presenter/FxALoginPresenterTest.kt
@@ -6,11 +6,14 @@
 
 package mozilla.lockbox.presenter
 
+import android.net.Uri
 import io.reactivex.Observable
-import io.reactivex.functions.Consumer
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.TestObserver
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.subjects.PublishSubject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import mozilla.lockbox.DisposingTest
 import mozilla.lockbox.action.AccountAction
 import mozilla.lockbox.action.LifecycleAction
 import mozilla.lockbox.flux.Action
@@ -31,24 +34,28 @@ import org.mockito.Mockito.`when` as whenCalled
 @ExperimentalCoroutinesApi
 @RunWith(RobolectricTestRunner::class)
 @PrepareForTest(AccountStore::class)
-class FxALoginPresenterTest {
-    class FakeFxALoginView : FxALoginView {
-        val webViewRedirects = PublishSubject.create<String>()
+class FxALoginPresenterTest : DisposingTest() {
+    class FakeFxALoginView(
+        val compositeDisposable: CompositeDisposable
+    ) : FxALoginView {
+        val webViewRedirectTo = PublishSubject.create<Uri>()
+        val webViewOverride = PublishSubject.create<Boolean?>()
         var loadedURL: String? = null
-        override var webViewObserver: Consumer<String?>?
-            get() = TODO("not implemented")
-            set(value) {
-                this.webViewRedirects.subscribe(value)
-            }
-
+        override var webViewRedirect: (url: Uri?) -> Boolean = { _ -> false }
         override fun loadURL(url: String) {
             loadedURL = url
         }
 
         override var skipFxAClicks: Observable<Unit> = PublishSubject.create<Unit>()
+
+        init {
+            webViewRedirectTo.subscribe {
+                webViewOverride.onNext(webViewRedirect(it))
+            }.addTo(compositeDisposable)
+        }
     }
 
-    val view = FakeFxALoginView()
+    val view = FakeFxALoginView(disposer)
 
     @Mock
     val accountStore = PowerMockito.mock(AccountStore::class.java)
@@ -70,6 +77,16 @@ class FxALoginPresenterTest {
     }
 
     @Test
+    fun `isRedirectURI is working as expected`() {
+        var url: String? = null
+        Assert.assertFalse(subject.isRedirectUri(url))
+        url = "https://www.mozilla.org/"
+        Assert.assertFalse(subject.isRedirectUri(url))
+        url = Constant.FxA.redirectUri + "?moz_fake"
+        Assert.assertTrue(subject.isRedirectUri(url))
+    }
+
+    @Test
     fun `onViewReady, when the accountStore pushes a new loginURL`() {
         val url = "www.mozilla.org"
         loginURLSubject.onNext(url)
@@ -79,17 +96,17 @@ class FxALoginPresenterTest {
 
     @Test
     fun `onViewReady, when the webview redirects to a URL starting with the expected redirect`() {
-        val url = Constant.FxA.redirectUri + "/moz_fake"
-        view.webViewRedirects.onNext(url)
+        val url = Uri.parse(Constant.FxA.redirectUri + "?moz_fake")
+        view.webViewRedirectTo.onNext(url)
 
         val redirectAction = dispatcherObserver.values().first() as AccountAction.OauthRedirect
-        Assert.assertEquals(url, redirectAction.url)
+        Assert.assertEquals(url.toString(), redirectAction.url)
     }
 
     @Test
     fun `onViewReady, when the webview redirects to a URL not starting with the expected redirect`() {
-        val url = "www.mozilla.org"
-        view.webViewRedirects.onNext(url)
+        val url = Uri.parse("https://www.mozilla.org")
+        view.webViewRedirectTo.onNext(url)
 
         dispatcherObserver.assertEmpty()
     }
