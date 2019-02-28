55dcceb07ed647dafcfc27d775229c45d39fb91b
sashei <sashaheinen@gmail.com>
2019-02-28 10:58:20 -0700

    routepresenter test working

--- app/src/test/java/mozilla/lockbox/presenter/AutofillRoutePresenterTest.kt
+++ app/src/test/java/mozilla/lockbox/presenter/AutofillRoutePresenterTest.kt
@@ -12,6 +12,11 @@ import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.PublishSubject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -23,20 +28,33 @@ import mozilla.lockbox.flux.Dispatcher
 import mozilla.lockbox.store.DataStore
 import mozilla.lockbox.store.RouteStore
 import mozilla.lockbox.support.PublicSuffixSupport
+import mozilla.lockbox.view.AutofillFilterFragment
 import mozilla.lockbox.view.FingerprintAuthDialogFragment
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mock
+import org.mockito.Mockito.anyString
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyZeroInteractions
 import org.powermock.api.mockito.PowerMockito
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 import org.powermock.api.mockito.PowerMockito.`when` as whenCalled
 
 @ExperimentalCoroutinesApi
 @RunWith(PowerMockRunner::class)
-@PrepareForTest(Navigation::class, FingerprintAuthDialogFragment::class)
+@PrepareForTest(
+    AutofillRoutePresenter::class,
+    Navigation::class,
+    FingerprintAuthDialogFragment::class,
+    AutofillFilterFragment::class
+)
 class AutofillRoutePresenterTest {
     @Mock
     val navController: NavController = mock(NavController::class.java)
@@ -45,7 +63,10 @@ class AutofillRoutePresenterTest {
     val navDestination: NavDestination = mock(NavDestination::class.java)
 
     @Mock
-    val fingerprintAuthDialogFragment: FingerprintAuthDialogFragment = mock(FingerprintAuthDialogFragment::class.java)
+    val fingerprintAuthDialogFragment: FingerprintAuthDialogFragment = PowerMockito.mock(FingerprintAuthDialogFragment::class.java)
+
+    @Mock
+    val autofillFilterFragment: AutofillFilterFragment = PowerMockito.mock(AutofillFilterFragment::class.java)
 
     @Mock
     val fragmentManager: FragmentManager = mock(FragmentManager::class.java)
@@ -76,6 +97,21 @@ class AutofillRoutePresenterTest {
 
     class FakePslSupport : PublicSuffixSupport()
 
+    private val immediate = object : Scheduler() {
+        override fun scheduleDirect(
+            run: Runnable,
+            delay: Long,
+            unit: TimeUnit
+        ): Disposable {
+            return super.scheduleDirect(run, 0, unit)
+        }
+
+        override fun createWorker(): Scheduler.Worker {
+            return ExecutorScheduler.ExecutorWorker(
+                Executor { it.run() })
+        }
+    }
+
     private val dispatcher = Dispatcher()
     private val routeStore = FakeRouteStore()
     private val dataStore = FakeDataStore()
@@ -88,14 +124,21 @@ class AutofillRoutePresenterTest {
     fun setUp() {
         dispatcher.register.subscribe(dispatcherObserver)
 
+        // needed to make `.observeOn(AndroidSchedulers.mainThread())` work
+        // this is unnecessary in other tests because we are using the
+        // robolectric testrunner there.
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediate }
+
 //        whenCalled(responseBuilder.buildFilteredFillResponse(eq(activity), anyList())).thenReturn(filteredFillResponse)
 //        whenCalled(responseBuilder.buildFallbackFillResponse(eq(activity))).thenReturn(fallbackFillResponse)
 
-        PowerMockito.whenNew(FingerprintAuthDialogFragment::class.java).withAnyArguments()
+        PowerMockito.whenNew(FingerprintAuthDialogFragment::class.java).withNoArguments()
             .thenReturn(fingerprintAuthDialogFragment)
+        PowerMockito.whenNew(AutofillFilterFragment::class.java).withNoArguments()
+            .thenReturn(autofillFilterFragment)
 
         whenCalled(activity.supportFragmentManager).thenReturn(fragmentManager)
-        whenCalled(navDestination.id).thenReturn(R.id.fragment_locked)
+        whenCalled(navDestination.id).thenReturn(R.id.fragment_null)
         whenCalled(navController.currentDestination).thenReturn(navDestination)
         PowerMockito.mockStatic(Navigation::class.java)
         whenCalled(Navigation.findNavController(activity, R.id.autofill_fragment_nav_host)).thenReturn(navController)
@@ -105,10 +148,26 @@ class AutofillRoutePresenterTest {
     }
 
     @Test
-    fun `locked routes do nothing (default screen)`() {
-//        routeStore.routeStub.onNext(RouteAction.LockScreen)
-//
-//        verifyZeroInteractions(activity)
-//        verifyZeroInteractions(navController)
+    fun `locked routes`() {
+        routeStore.routeStub.onNext(RouteAction.LockScreen)
+
+        verify(navController).navigate(R.id.action_to_locked, null)
+    }
+
+    @Test
+    fun `item list routes navigate to search`() {
+        routeStore.routeStub.onNext(RouteAction.ItemList)
+
+        verify(autofillFilterFragment).show(eq(fragmentManager), anyString())
+        verify(autofillFilterFragment).setupDialog(R.string.autofill, null)
+    }
+
+    @Test
+    fun `fingerprint dialog routes navigate to fingerprint dialog`() {
+        val title = R.string.fingerprint_dialog_title
+        routeStore.routeStub.onNext(RouteAction.DialogFragment.FingerprintDialog(title))
+
+        verify(fingerprintAuthDialogFragment).show(eq(fragmentManager), anyString())
+        verify(fingerprintAuthDialogFragment).setupDialog(title, null)
     }
 }
