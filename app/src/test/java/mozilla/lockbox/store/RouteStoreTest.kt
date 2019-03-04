41b8ace0df6783d44ab88c19bfa44a7a226b0c67
Gustavo Fão <faogustavo@gmail.com>
2019-03-04 14:38:54 -0300

    refactor RouteStoreTest

--- app/src/test/java/mozilla/lockbox/store/RouteStoreTest.kt
+++ app/src/test/java/mozilla/lockbox/store/RouteStoreTest.kt
@@ -10,72 +10,42 @@ import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.PublishSubject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import mozilla.lockbox.action.DataStoreAction
 import mozilla.lockbox.action.LifecycleAction
 import mozilla.lockbox.action.OnboardingStatusAction
 import mozilla.lockbox.action.RouteAction
+import mozilla.lockbox.extensions.assertLastValue
 import mozilla.lockbox.flux.Action
 import mozilla.lockbox.flux.Dispatcher
-import mozilla.lockbox.mocks.MockDataStoreSupport
-import mozilla.lockbox.support.AutoLockSupport
-import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
 
 @ExperimentalCoroutinesApi
 class RouteStoreTest {
-    class FakeLifecycleStore : LifecycleStore() {
-        override val lifecycleEvents: Observable<LifecycleAction> = PublishSubject.create()
+
+    class FakeDataStore : DataStore() {
+        val stateStub = PublishSubject.create<DataStore.State>()
+
+        override val state: Observable<State>
+            get() = stateStub
     }
-
-    class FakeAutolockSupport : AutoLockSupport() {
-        var shouldLockStub: Boolean = false
-        override val shouldLock
-            get() = shouldLockStub
-
-        override fun storeNextAutoLockTime() {
-        }
-
-        override fun backdateNextLockTime() {
-        }
-
-        override fun forwardDateNextLockTime() {
-        }
-    }
-
-    private val support = MockDataStoreSupport()
 
     val dispatcher = Dispatcher()
     private val dispatcherObserver = TestObserver.create<Action>()
     private val routeObserver = TestObserver.create<RouteAction>()
-    private val dataStoreObserver = TestObserver.create<DataStore.State>()
-    private val autoLockSupport = Mockito.spy(FakeAutolockSupport())
-    private val lifecycleStore = FakeLifecycleStore()
-
-    private val dataStore = DataStore(dispatcher, support, autoLockSupport, lifecycleStore)
-    private val errorDataStore = DataStore(dispatcher)
+    private val dataStore = FakeDataStore()
 
     lateinit var subject: RouteStore
 
-    private fun setUpSubject() {
+    @Before
+    fun setUp() {
         subject = RouteStore(dispatcher, dataStore)
-        setupObservers()
-    }
 
-    private fun setUpErrorSubject() {
-        subject = RouteStore(dispatcher, errorDataStore)
-        setupObservers()
-    }
-
-    private fun setupObservers() {
-        dataStore.state.subscribe(dataStoreObserver)
         subject.routes.subscribe(routeObserver)
         dispatcher.register.subscribe(dispatcherObserver)
     }
 
     @Test
     fun `dispatched routeactions`() {
-        setUpSubject()
         val action = RouteAction.Welcome
         dispatcher.dispatch(action)
 
@@ -84,7 +54,6 @@ class RouteStoreTest {
 
     @Test
     fun `dispatched non-routeactions`() {
-        setUpSubject()
         val action = LifecycleAction.UserReset
         dispatcher.dispatch(action)
 
@@ -92,29 +61,19 @@ class RouteStoreTest {
     }
 
     @Test
-    fun `datastore failed to create backend`() {
-        setUpErrorSubject()
-
-        dispatcher.dispatch(DataStoreAction.Sync)
-        dispatcher.dispatch(OnboardingStatusAction(false))
-
-        routeObserver.assertEmpty()
-    }
-
-    @Test
     fun `dispatched on datastore`() {
-        setUpSubject()
-        val routesIterator = subject.routes.blockingIterable().iterator()
-
         dispatcher.dispatch(OnboardingStatusAction(false))
 
-        dispatcher.dispatch(DataStoreAction.Reset)
-        Assert.assertEquals(routesIterator.next(), RouteAction.Welcome)
+        dataStore.stateStub.onNext(DataStore.State.Errored(Exception("Fake exception for testing purpose")))
+        routeObserver.assertEmpty()
 
-        dispatcher.dispatch(DataStoreAction.Unlock)
-        Assert.assertEquals(routesIterator.next(), RouteAction.ItemList)
+        dataStore.stateStub.onNext(DataStore.State.Unprepared)
+        routeObserver.assertLastValue(RouteAction.Welcome)
 
-        dispatcher.dispatch(DataStoreAction.Lock)
-        Assert.assertEquals(routesIterator.next(), RouteAction.LockScreen)
+        dataStore.stateStub.onNext(DataStore.State.Unlocked)
+        routeObserver.assertLastValue(RouteAction.ItemList)
+
+        dataStore.stateStub.onNext(DataStore.State.Locked)
+        routeObserver.assertLastValue(RouteAction.LockScreen)
     }
 }
