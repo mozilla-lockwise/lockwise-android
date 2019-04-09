b71b927d1605cb027b3ab897140c98472e9aef20
Christoph Loy <loy.christoph@gmail.com>
2019-04-09 21:18:16 +0200

    Fix AndroidLint by updating dependencies (#572)
    * Update recylcerview, appcompat and lifecycle to alpha-04
    
    * Remove deprecated method calls and replace them by suggested alternatives
    
    * Reintroduce fixes for deprecated methods in RoutePresenter
    
    * Re-Add Codestyle for Obj-C

--- app/src/main/java/mozilla/lockbox/presenter/RoutePresenter.kt
+++ app/src/main/java/mozilla/lockbox/presenter/RoutePresenter.kt
@@ -4,8 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-@file:Suppress("DEPRECATION")
-
 package mozilla.lockbox.presenter
 
 import android.app.KeyguardManager
@@ -16,6 +14,7 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.arch.core.util.Cancellable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
@@ -57,15 +56,18 @@ abstract class RoutePresenter(
         false
     }
 
+    private var backListenerCancellable: Cancellable? = null
+
     override fun onPause() {
         super.onPause()
-        activity.removeOnBackPressedCallback(backListener)
+        backListenerCancellable?.cancel()
+        backListenerCancellable = null
         compositeDisposable.clear()
     }
 
     override fun onResume() {
         super.onResume()
-        activity.addOnBackPressedCallback(backListener)
+        backListenerCancellable = activity.onBackPressedDispatcher.addCallback(activity, backListener)
     }
 
     protected abstract fun route(action: RouteAction)
