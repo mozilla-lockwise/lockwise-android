fb11962499b8ccf04939a5cce52ccf16f75a5cb1
sashei <sashaheinen@gmail.com>
2019-05-02 12:05:30 -0700

    set flag_secure for release builds on autofill

--- app/src/main/java/mozilla/lockbox/view/AutofillRootActivity.kt
+++ app/src/main/java/mozilla/lockbox/view/AutofillRootActivity.kt
@@ -3,11 +3,13 @@ package mozilla.lockbox.view
 import android.annotation.TargetApi
 import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import mozilla.lockbox.R
 import mozilla.lockbox.autofill.IntentBuilder
 import mozilla.lockbox.presenter.AutofillRoutePresenter
+import mozilla.lockbox.support.isDebug
 
 @TargetApi(Build.VERSION_CODES.O)
 @ExperimentalCoroutinesApi
@@ -17,6 +19,7 @@ class AutofillRootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_autofill)
+        if (!isDebug()) window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
 
         // pull responsebuilder & search status out of intent at a later iteration of this
         val responseBuilder = IntentBuilder.getResponseBuilder(intent)
