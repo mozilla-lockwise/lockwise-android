abcb6ed2105ee2f7ab88aa7598f0945c7170b4b7
sashei <sashaheinen@gmail.com>
2019-06-05 13:12:49 -0700

    swap working, tests started

--- app/src/test/java/mozilla/lockbox/store/FingerprintStoreTest.kt
+++ app/src/test/java/mozilla/lockbox/store/FingerprintStoreTest.kt
@@ -11,24 +11,178 @@ package mozilla.lockbox.store
 import android.app.KeyguardManager
 import android.content.Context
 import android.hardware.fingerprint.FingerprintManager
+import android.hardware.fingerprint.FingerprintManager.FINGERPRINT_ERROR_LOCKOUT
+import android.os.CancellationSignal
+import android.preference.PreferenceManager
+import android.util.Base64
+import io.reactivex.observers.TestObserver
+import mozilla.components.lib.dataprotect.Keystore
+import mozilla.lockbox.action.FingerprintSensorAction
+import mozilla.lockbox.flux.Dispatcher
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.never
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyZeroInteractions
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.api.mockito.PowerMockito.doReturn
+import org.powermock.api.mockito.PowerMockito.verifyStatic
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import javax.crypto.Cipher
 import org.mockito.Mockito.`when` as whenCalled
 
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(Keystore::class, FingerprintManager::class)
 @Suppress("DEPRECATION")
 class FingerprintStoreTest {
     @Mock
-    val fingerprintManager = Mockito.mock(FingerprintManager::class.java)
+    val keystore: Keystore = PowerMockito.mock(Keystore::class.java)
 
     @Mock
-    val keyguardManager = Mockito.mock(KeyguardManager::class.java)
+    val cipher: Cipher = PowerMockito.mock(Cipher::class.java)
 
     @Mock
-    val context = Mockito.mock(Context::class.java)
+    val fingerprintManager: FingerprintManager = Mockito.mock(FingerprintManager::class.java)
 
-    val subject = FingerprintStore()
+    @Mock
+    val keyguardManager: KeyguardManager = Mockito.mock(KeyguardManager::class.java)
+
+    @Mock
+    val context: Context = Mockito.mock(Context::class.java)
+
+    private val dispatcher = Dispatcher()
+    val subject = FingerprintStore(dispatcher, keystore)
+
+    @Before
+    fun setUp() {
+        whenCalled(keystore.createEncryptCipher()).thenReturn(cipher)
+    }
+
+    @Test
+    fun `fingerprint start sensor action when fingerprints are not available`() {
+        whenCalled(fingerprintManager.isHardwareDetected).thenReturn(false)
+        whenCalled(fingerprintManager.hasEnrolledFingerprints()).thenReturn(false)
+        applyMockContext()
+
+        dispatcher.dispatch(FingerprintSensorAction.Start)
+
+        verifyZeroInteractions(keystore)
+    }
+
+    @Test
+    fun `fingerprint start sensor action when fingerprints are available`() {
+        whenCalled(fingerprintManager.isHardwareDetected).thenReturn(true)
+        whenCalled(fingerprintManager.hasEnrolledFingerprints()).thenReturn(true)
+        applyMockContext()
+
+        val callbackCaptor = ArgumentCaptor.forClass(FingerprintStore.AuthenticationCallback::class.java)
+
+        dispatcher.dispatch(FingerprintSensorAction.Start)
+
+        verify(keystore).createEncryptCipher()
+        verify(fingerprintManager).authenticate(
+            any(FingerprintManager.CryptoObject::class.java),
+            any(CancellationSignal::class.java),
+            eq(0),
+            callbackCaptor.capture(),
+            eq(null)
+        )
+    }
+
+    @Test
+    fun `callbacks with lockout authentication errors`() {
+        whenCalled(fingerprintManager.isHardwareDetected).thenReturn(true)
+        whenCalled(fingerprintManager.hasEnrolledFingerprints()).thenReturn(true)
+        applyMockContext()
+
+        val callbackCaptor = ArgumentCaptor.forClass(FingerprintStore.AuthenticationCallback::class.java)
+
+        dispatcher.dispatch(FingerprintSensorAction.Start)
+
+        verify(keystore).createEncryptCipher()
+        verify(fingerprintManager).authenticate(
+            any(FingerprintManager.CryptoObject::class.java),
+            any(CancellationSignal::class.java),
+            eq(0),
+            callbackCaptor.capture(),
+            eq(null)
+        )
+
+        val stateObserver = TestObserver.create<FingerprintStore.AuthenticationState>()
+        subject.authState.subscribe(stateObserver)
+
+        callbackCaptor.value.onAuthenticationError(FINGERPRINT_ERROR_LOCKOUT, null)
+
+        
+    }
+
+    @Test
+    fun `callbacks with authentication succeeded`() {
+        whenCalled(fingerprintManager.isHardwareDetected).thenReturn(true)
+        whenCalled(fingerprintManager.hasEnrolledFingerprints()).thenReturn(true)
+        applyMockContext()
+
+        val callbackCaptor = ArgumentCaptor.forClass(FingerprintStore.AuthenticationCallback::class.java)
+
+        dispatcher.dispatch(FingerprintSensorAction.Start)
+
+        verify(keystore).createEncryptCipher()
+        verify(fingerprintManager).authenticate(
+            any(FingerprintManager.CryptoObject::class.java),
+            any(CancellationSignal::class.java),
+            eq(0),
+            callbackCaptor.capture(),
+            eq(null)
+        )
+    }
+
+    @Test
+    fun `callbacks with authentication help`() {
+        whenCalled(fingerprintManager.isHardwareDetected).thenReturn(true)
+        whenCalled(fingerprintManager.hasEnrolledFingerprints()).thenReturn(true)
+        applyMockContext()
+
+        val callbackCaptor = ArgumentCaptor.forClass(FingerprintStore.AuthenticationCallback::class.java)
+
+        dispatcher.dispatch(FingerprintSensorAction.Start)
+
+        verify(keystore).createEncryptCipher()
+        verify(fingerprintManager).authenticate(
+            any(FingerprintManager.CryptoObject::class.java),
+            any(CancellationSignal::class.java),
+            eq(0),
+            callbackCaptor.capture(),
+            eq(null)
+        )
+    }
+
+    @Test
+    fun `callbacks with authentication failed`() {
+        whenCalled(fingerprintManager.isHardwareDetected).thenReturn(true)
+        whenCalled(fingerprintManager.hasEnrolledFingerprints()).thenReturn(true)
+        applyMockContext()
+
+        val callbackCaptor = ArgumentCaptor.forClass(FingerprintStore.AuthenticationCallback::class.java)
+
+        dispatcher.dispatch(FingerprintSensorAction.Start)
+
+        verify(keystore).createEncryptCipher()
+        verify(fingerprintManager).authenticate(
+            any(FingerprintManager.CryptoObject::class.java),
+            any(CancellationSignal::class.java),
+            eq(0),
+            callbackCaptor.capture(),
+            eq(null)
+        )
+    }
 
     @Test
     fun `isDeviceSecure when the device is fingerprint secure and not PIN or password secure`() {
