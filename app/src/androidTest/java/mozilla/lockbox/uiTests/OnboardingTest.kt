01f5a6a6afa3937cfe07c0686d1eefa2c3b8cd08
Isabel Rios <isabelrios@MackBookirios.home>
2019-06-05 21:03:50 +0200

    importing all robots

--- app/src/androidTest/java/mozilla/lockbox/uiTests/OnboardingTest.kt
+++ app/src/androidTest/java/mozilla/lockbox/uiTests/OnboardingTest.kt
@@ -7,6 +7,7 @@ import mozilla.lockbox.robots.autofillOnboardingScreen
 import mozilla.lockbox.robots.fingerprintOnboardingScreen
 import mozilla.lockbox.robots.fxaLogin
 import mozilla.lockbox.robots.onboardingConfirmationScreen
+import mozilla.lockbox.robots.disconnectDisclaimer
 import mozilla.lockbox.store.FingerprintStore
 import mozilla.lockbox.store.SettingStore
 import mozilla.lockbox.view.RootActivity
