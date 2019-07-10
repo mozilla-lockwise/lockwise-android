6fe2146b01f471ad84993044011fa2e8fa46835c
Elise Richards <erichards@mozilla.com>
2019-07-10 07:57:28 -0700

    810: Reset support when syncing (#826)
    * Reset support when info is null
    
    * Reset support every time we sync.

--- app/src/main/java/mozilla/lockbox/store/DataStore.kt
+++ app/src/main/java/mozilla/lockbox/store/DataStore.kt
@@ -236,15 +236,12 @@ open class DataStore(
     }
 
     private fun sync() {
-        val syncConfig = support.syncConfig ?: return {
-            val throwable = IllegalStateException("syncConfig should already be defined")
-            pushError(throwable)
-        }()
+        resetSupport(support)
 
         // ideally, we don't sync unless we are connected to the network
         syncStateSubject.accept(SyncState.Syncing)
 
-        backend.sync(syncConfig)
+        backend.sync(support.syncConfig!!)
             .asSingle(coroutineContext)
             .timeout(Constant.App.syncTimeout, TimeUnit.SECONDS)
             .doOnEvent { _, err ->
@@ -326,7 +323,12 @@ open class DataStore(
         }
 
         when (loginsException) {
-            is SyncAuthInvalidException, is InvalidKeyException -> dispatcher.dispatch(LifecycleAction.UserReset)
+            is SyncAuthInvalidException,
+            is InvalidKeyException,
+            is LoginsStorageException -> {
+                resetSupport(support)
+                dispatcher.dispatch(LifecycleAction.UserReset)
+            }
         }
     }
 
