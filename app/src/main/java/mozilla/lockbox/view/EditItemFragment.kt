21fb43e0dcdd8db02fedc4c1f58f4de85e0eb8ba
Elise Richards <erichards@mozilla.com>
2019-09-17 16:37:30 -0500

    Edit view styling

--- app/src/main/java/mozilla/lockbox/view/EditItemFragment.kt
+++ app/src/main/java/mozilla/lockbox/view/EditItemFragment.kt
@@ -145,7 +145,7 @@ class EditItemFragment : BackableFragment(), EditItemDetailView {
                     R.id.inputLayoutUsername -> {
                         when {
                             TextUtils.isEmpty(inputText) -> {
-                                errorLayout.helperText = context?.getString(R.string.hint_username)
+                                errorLayout.error = null
                             }
                         }
                     }
