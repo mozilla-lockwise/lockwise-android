66b6b3e51671e996cf5cad1627ab36b1797b9322
Elise Richards <erichards@mozilla.com>
2019-11-14 14:11:05 -0600

    Move close and open keyboard methods to parent fragment

--- app/src/main/java/mozilla/lockbox/view/AppWebPageFragment.kt
+++ app/src/main/java/mozilla/lockbox/view/AppWebPageFragment.kt
@@ -7,23 +7,21 @@
 package mozilla.lockbox.view
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.annotation.StringRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import mozilla.lockbox.R
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.StringRes
 import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.fragment_fxa_login.view.*
 import kotlinx.android.synthetic.main.fragment_webview.*
 import kotlinx.android.synthetic.main.include_backable.view.*
-import mozilla.lockbox.presenter.WebPageView
+import mozilla.lockbox.R
 import mozilla.lockbox.presenter.AppWebPagePresenter
+import mozilla.lockbox.presenter.WebPageView
 
 class AppWebPageFragment : BackableFragment(), WebPageView {
 
@@ -56,14 +54,7 @@ class AppWebPageFragment : BackableFragment(), WebPageView {
 
     override fun onPause() {
         super.onPause()
-        closeKeyboard()
-    }
-
-    private fun closeKeyboard() {
-        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        if (imm.isAcceptingText) {
-            imm.hideSoftInputFromWindow(view!!.windowToken, 0)
-        }
+        closeKeyboard(view)
     }
 
     override fun loadURL(url: String) {
@@ -79,14 +70,14 @@ class AppWebPageFragment : BackableFragment(), WebPageView {
     override fun handleNetworkError(networkErrorVisibility: Boolean) {
         if (!networkErrorVisibility) {
             errorHelper.showNetworkError(
-                parent = view!!,
-                child = view!!.webView,
+                parent = view,
+                child = view?.webView,
                 topMarginId = R.dimen.network_error
             )
         } else {
             errorHelper.hideNetworkError(
-                parent = view!!,
-                child = view!!.webView,
+                parent = view,
+                child = view?.webView,
                 topMarginId = R.dimen.hidden_network_error
             )
         }
